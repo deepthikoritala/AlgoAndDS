1f8f0d4777b6a6569514d5aa93680f0905db1a19
deepthi koritala <deepthikoritala@gmail.com>
2024-04-11 16:50:29 -0500

    added comments

--- SievePrime.cs
+++ SievePrime.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using System.Text;
-using System.Threading.Tasks;
+
+//A = 9
+//[2, 7]
+
+
+//A = 20
+//[3, 17]
 
 namespace ConsoleApp1
 {
@@ -24,6 +29,8 @@ namespace ConsoleApp1
             for (int i = 2; i * i < A; i++) {
 
                 if (isprime[i] == true) {
+                    //here making all multiples of numbers to not prime like if 2 is prime -> 4,6,8 are not prime
+                    //3 is prime -> 9, 12,15 are not prime
                     for (int j = i*i; j < A; j+=i)
                     {
                         isprime[j] = false;
